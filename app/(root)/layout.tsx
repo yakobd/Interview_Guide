982a5448e0081a418b7802fded8cd961a9a5c342
Yakob Dereje <yakobdereje.yd@gmail.com>
2025-11-01 08:23:09 -0700

    Auth page done!

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,8 @@
+import React, { ReactNode } from "react";
+import RootLayout from "../layout";
+
+const Rootlayout = ({ children }: { children: ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+export default RootLayout;
