9f7d0d0a945c996d86471a160785eb9ff7d063f1
Yakob Dereje <yakobdereje.yd@gmail.com>
2025-11-03 06:38:24 -0800

    Navigation bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,21 @@
 import React, { ReactNode } from "react";
 import RootLayout from "../layout";
+import Link from "next/link";
+import Image from "next/image";
 
 const Rootlayout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="root-layout">
+      {/* Navigation  */}
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="Logo" width={38} height={32} />
+          <h2 className="text-primary-100">Company Name</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
+  );
 };
 
-export default RootLayout;
+export default Rootlayout;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,42 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
-  return <div>Home Page</div>;
+  return (
+    <>
+      {/* Top header card  */}
+
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            {" "}
+            Practice on real interview questions & get instant feedback
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview"> Start an Interview</Link>
+          </Button>
+        </div>
+        <Image
+          src="/robot.png"
+          alt="robot"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+
+      {/* Interview List Part  */}
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section">
+          <p>You haven't taken any interviews yet</p>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default page;
